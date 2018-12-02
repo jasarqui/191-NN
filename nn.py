2f2e21627cd79825e1463365546e76160268e0f0
jasarqui <jzarquilita@up.edu.ph>
2018-12-02 20:22:27 +0800

    Add Backward Propagation and Training

--- nn.py
+++ nn.py
@@ -1,10 +1,26 @@
 import numpy as np
-import torch
 import csv
+
+'''
+AUTHOR:
+Arquilita, Jasper Ian Z.
+CMSC 191-Z - Introduction to Neural Networks.
+
+DESCRIPTION:
+A 1-3-1 Neural Network that predicts the 
+Relative Humidity on a certain day.
+
+DATASET:
+ArcGIS Data Access Viewer. NASA. Retrieved from:
+https://power.larc.nasa.gov/data-access-viewer/
+'''
 
 # constants
 NUM_NEURON = 3
 NUM_INPUTS = 2
+NUM_EPOCHS = 1000
+NUM_MCHEPS = 0.01
+BIAS_GAMMA = 0.5
 
 # get dataset
 with open('weather_data.csv') as csv_file:
@@ -17,21 +33,103 @@ with open('weather_data.csv') as csv_file:
         else: data = np.vstack((data, np.array(row).astype(float)))
         
     latitude, longitude = data[0,0], data[0,1] # get coordinates
-    trueval = data[:, 5] # get the true value
+    trueval = data[:, 5] / 100 # get the true values and normalize
     data = np.delete(data, 5, 1)[:, 4:6] # get the variables
+    data = np.transpose(data) # change variables by row
 
-# initial memory
-weights = torch.from_numpy(np.zeros((NUM_NEURON, NUM_INPUTS)))
-bias = torch.from_numpy(np.zeros((NUM_NEURON, NUM_INPUTS)))
+# Initial Memory
+hidden_weights = np.zeros((NUM_INPUTS, NUM_NEURON)) + 0.1
+output_weights = np.zeros(NUM_INPUTS) + 0.1
+bias = np.zeros((NUM_INPUTS, NUM_NEURON)) + 0.1
+output_bias = np.array([0.1])
+
+# Activation Functions
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return x * (x > 0)
+
+# Activation Function Derivatives
+def dsigmoid(x):
+    return x * (1 - x)
+
+def drelu(x):
+    if x <= 0: return 0
+    else: return 1
 
 # Forward Pass
-def forward_pass(data, weights, bias, layer):
-    return torch.relu(torch.matmul(torch.t(weights[layer]), data) + bias)
-
-# midterm exam
-# w = torch.from_numpy(np.array([0.16, 0.73]))
-# a0 = torch.from_numpy(np.array([0.33, 0.59]))
-# a = torch.from_numpy(np.array([[0.01, 0.44], [0.03, 0.23]]))
-# print(forward_pass(w, a, a0, 1))
+def forward_pass(data, weights, bias, layer, is_output):
+    if is_output: return sigmoid(np.matmul(np.transpose(weights)[layer], data) + bias)
+    else:
+        result = np.matmul(np.transpose(weights), data)
+        return relu(np.matmul(np.transpose(weights), data))
 
 # Backward Propagation
+def backward_prop(data, hidden_weights, output_weights, bias, output_bias, hidden_layer_outputs, trueval, result):
+    # the effect of output to error
+    error_output = result - trueval
+    error_output_delta = error * dsigmoid(result)
+
+    # the effect of hidden layer 3 to error
+    error_layer3 = np.matmul(error_output_delta, np.transpose(hidden_weights)[2])
+    error_layer3_delta = error_layer3 * drelu(hidden_layer_outputs[2]) 
+
+    # the effect of hidden layer 2 to error
+    error_layer2 = np.matmul(error_layer3_delta, np.transpose(hidden_weights)[1])
+    error_layer2_delta = error_layer2 * drelu(hidden_layer_outputs[1])
+
+    # the effect of hidden layer 1 to error
+    error_layer1 = np.matmul(error_layer2_delta, np.transpose(hidden_weights)[0])
+    error_layer1_delta = error_layer1 * drelu(hidden_layer_outputs[0])
+
+    # adjust the weights
+    hidden_weights[0] += np.matmul(data, error_layer1_delta)
+    hidden_weights[1] += np.matmul(np.transpose(hidden_layer_outputs[0]), error_layer2_delta)
+    hidden_weights[2] += np.matmul(np.transpose(hidden_layer_outputs[1]), error_layer3_delta)
+    output_weights += np.matmul(np.transpose(hidden_layer_outputs), error_output_delta)
+
+    # adjust bias
+    output_bias -= BIAS_GAMMA * error_output_delta
+    bias[0] -= BIAS_GAMMA * error_layer1_delta
+    bias[1] -= BIAS_GAMMA * error_layer2_delta
+    bias[2] -= BIAS_GAMMA * error_layer3_delta
+
+    return (hidden_weights, output_weights, bias, output_bias)
+
+# Initialize Variables
+# for loop checking
+result = np.zeros(len(data[0]))
+curr_epoch = 0
+loss = 100
+
+# Perform Training
+# ends if max epochs is reached or tolerance is reached
+while (curr_epoch < NUM_EPOCHS and loss > NUM_MCHEPS):
+    is_tolerable = True # true until proven false
+
+    for datum_index in (0, len(data[0])):
+        hidden_layer_outputs = np.array([])
+        result[datum_index] = data[datum_index] # initialize as the starting variable value
+
+        for layer in (0, NUM_NEURON + 1): # + 1 to include the output layer
+            # this will let the variables pass through the neurons
+            # as it changes value
+            if layer != NUM_NEURON:
+                hidden_layer_outputs = forward_pass(result, hidden_weights, bias[layer], layer, False)
+                if layer == 0: hidden_layer_outputs = result[datum_index]
+                else: hidden_layer_outputs = np.vstack((hidden_layer_outputs, result[datum_index]), 0)
+            else: result[datum_index] = forward_pass(result, output_weights, output_bias, layer, True) # output layer
+    
+    # calculate loss
+    error = result - trueval
+    loss = np.sum(0.5 * (error ** 2))
+    print("EPOCH {} LOSS {}".format(curr_epoch, loss))
+
+    # perform backward propagation
+    (hidden_weights, output_weights, bias, output_bias) = backward_prop(data, hidden_weights, output_weights, bias, output_bias, hidden_layer_outputs, trueval, result)
+    
+    # update
+    curr_epoch += 1
+
+print("Training done.")
